8d76e48390ec204916e440a050231799c863a5e3
Saravenpi <saravenpi@tuta.io>
2026-05-14 06:35:43 +0200

    :recycle: use renamed tracker path s.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
     return (
         <html lang={locale} className="bg-background">
         <head>
-            <script defer src="https://vision.facile.studio/t.js?v=2"></script>
+            <script defer src="https://vision.facile.studio/s.js"></script>
         </head>
         <body className={manrope.variable}>
         <NextIntlClientProvider messages={messages} locale={locale}>
