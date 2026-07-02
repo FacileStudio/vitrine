e2c1050da81bef6c2148cc035252cfa1a4f79450
G1anC <noah.steiniger@epitech.eu>
2026-07-02 17:32:24 +0200

    fix (responsive): start of responsive on home hero section

--- app/[locale]/homeSections/hero.tsx
+++ app/[locale]/homeSections/hero.tsx
@@ -14,7 +14,7 @@ export default function Hero({ charged }: { charged: boolean }) {
     const paraSentinel = useRef<HTMLDivElement>(null);
     const textExitSentinel = useRef<HTMLDivElement>(null);
     const lineRefs = useRef<(HTMLElement | null)[]>([]);
-    const ctaRef = useRef<HTMLButtonElement>(null);
+    const ctaRef = useRef<HTMLAnchorElement>(null);
     const [showText, setShowText] = useState(false);
     const [leaving, setLeaving] = useState(false);
     const [resolved, setResolved] = useState(false);
@@ -56,39 +56,58 @@ export default function Hero({ charged }: { charged: boolean }) {
             <div className="relative w-screen h-screen">
                 <DitherView
                     file="/models/fff.glb"
-                    className="absolute top-0 left-0 w-full h-full -z-10 opacity-75"
-                    background="#9DBAAD"
-                    highlight="#24E27A"
-                    grayscaleOnly={false}
-                    intensity={1.8}
+                    className="absolute top-0 left-0 w-full h-full -z-10 lg:opacity-75 opacity-25"
                     gridSize={resolved ? 2 : 16}
                     position={[-1, -0.5, -0.5]}
-                    fov={50}
-                    scale={35}
-                    float={false}
-                    parallax={0.55}
                     rotation={[0, 0.35, 0]}
+                    grayscaleOnly={false}
+                    background="#9DBAAD"
+                    highlight="#24E27A"
+                    parallax={0.55}
+                    intensity={1.8}
+                    float={false}
+                    scale={35}
+                    fov={50}
                 />
                 <div className="absolute bottom-0 left-0 w-full h-1/5 bg-linear-to-t from-[#E4EEE8] to-transparent" />
             </div>
 
-            <div className="absolute top-2/5 w-full -translate-y-1/2 text-center flex items-start px-15">
-                <div className="w-full text-left">
-                    <span className="block overflow-hidden"><div ref={(el) => { lineRefs.current[0] = el; }} className="italic text-lg">[fasil]</div></span>
-                    <span className="block overflow-hidden mt-2"><div ref={(el) => { lineRefs.current[1] = el; }} className="opacity-50 font-medium text-xs">Qui se fait sans effort, qui ne presente aucune</div></span>
-                    <span className="block overflow-hidden"><div ref={(el) => { lineRefs.current[2] = el; }} className="opacity-50 font-medium text-xs">difficulté. Simple, aise, etc...</div></span>
+            <div className="absolute top-2/5 w-full -translate-y-1/2 text-center flex flex-col lg:flex-row lg:gap-0 items-start lg:px-15 px-4">
+                <div className="w-full text-left mb-12 font-medium text-xs">
+                    <span className="block overflow-hidden"><div ref={(el) => { lineRefs.current[0] = el; }} className="italic lg:text-lg text-lg">[fasil]</div></span>
+                    <span className="block overflow-hidden mt-2"><div ref={(el) => { lineRefs.current[1] = el; }} className="opacity-50 ">Qui se fait sans effort, qui ne presente aucune</div></span>
+                    <span className="block overflow-hidden"><div ref={(el) => { lineRefs.current[2] = el; }} className="opacity-50">difficulté. Simple, aise, etc...</div></span>
                 </div>
 
-                <div className="w-full text-left ">
-                    <span className="block overflow-hidden"><div ref={(el) => { lineRefs.current[3] = el; }} className="text-2xl font-medium">Quand 2 bons designers rencontrent</div></span>
-                    <span className="block overflow-hidden"><div ref={(el) => { lineRefs.current[4] = el; }} className="text-2xl font-medium">2 bons developpeurs</div></span>
-                    <button ref={ctaRef} className="mt-6 px-6 py-4 border-2 border-black/10 text-xs font-medium rounded-full">Voir nos projets</button>
+                <div className="w-full text-left xl:text-2xl lg:text-xl text-lg">
+                    <span className="block overflow-hidden"><div ref={(el) => { lineRefs.current[3] = el; }} className="font-medium">Quand 2 bons designers rencontrent</div></span>
+                    <span className="block overflow-hidden"><div ref={(el) => { lineRefs.current[4] = el; }} className="font-medium">2 bons developpeurs</div></span>
+                    <a ref={ctaRef} href="/projects" className="mt-6 px-6 py-4 border-2 border-black/10 text-xs font-medium rounded-full bg-background/50">Voir nos projets</a>
                 </div>
             </div>
 
-            <div className="absolute bottom-0 flex items-end justify-between w-full px-15 pb-12">
-                <span className="block overflow-hidden w-1/2"><img ref={(el) => { lineRefs.current[5] = el; }} src="/Facile.svg" alt="Facile Logo" className="w-full aspect-auto max-h-[33vh]" /></span>
-                <span className="block overflow-hidden"><span ref={(el) => { lineRefs.current[6] = el; }} className="block font-medium text-4xl">Studio</span></span>
+            <div className="absolute bottom-0 flex flex-col-reverse lg:flex-row items-end justify-between w-full p-4 lg:px-15 lg:b-12">
+                <span className="block overflow-hidden lg:w-1/2 w-full">
+                    <img
+                        ref={(el) => {
+                            lineRefs.current[5] = el;
+                        }}
+                        src="/Facile.svg"
+                        alt="Facile Logo"
+                        className="w-full aspect-auto max-h-[33vh]"
+                    />
+                </span>
+
+                <span className="block overflow-hidden">
+                    <span
+                        ref={(el) => {
+                            lineRefs.current[6] = el;
+                        }}
+                        className="block font-medium text-2xl mt-4 lg:text-4xl"
+                    >
+                        Studio
+                    </span>
+                </span>
             </div>
 
             <div ref={paraSentinel}     className="absolute top-[10%] w-full h-px" aria-hidden="true" />
